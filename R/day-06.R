8803f9e6516764947b11cd20129521f71d693a33
Hindstein <ianhinds@ucsb.edu>
2020-08-11 20:18:13 -0700

    daily 06 initial

---
+++ R/day-06.R
@@ -0,0 +1,7 @@
+# Author: "Ian Hinds"
+# Date: 2020-08-11
+# Purpose: daily exercise 6: Using ggplot with COVID Data
+
+library(tidyverse)
+
+url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
